4defad50b00a018509f1b8d91b3f21d44375138e
Dylan185-cpu <dylan.estee@gmail.com>
2026-05-28 15:34:26 +1000

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,11 @@
+print("Hello,world!")
+
+#printing my name
+print("my name is Dylan.")
+
+#printing a fun fact about myself
+# print("I love playing basketball.")
+print("I love playing basketball.")
+
+#performing and printing a simple calculation
+print ("result of 10 + 5 is:",10 + 5)
