763f4a9c006cbd57049f4ac287740d5f1584581b
Dylan185-cpu <dylan.estee@gmail.com>
2026-06-25 15:27:17 +1000

    Add files via upload

---
+++ project6.py
@@ -0,0 +1,12 @@
+num1 = 10
+num2 = 20
+
+print("num1 =", num1)
+print("num2 =", num2)
+
+print("num1 == num2:", num1 == num2)  # Equal to
+print("num1 != num2:", num1 != num2)  # Not equal to
+print("num1 > num2:", num1 > num2)    # Greater than
+print("num1 < num2:", num1 < num2)    # Less than
+print("num1 >= num2:", num1 >= num2)  # Greater than or equal to
+print("num1 <= num2:", num1 <= num2)  # Less than or equal to
